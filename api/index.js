cab0baabe0a736980569cdaab7c7bdc0f60048c3
Yash Bhatt <30y.bhatt@gmail.com>
2024-08-10 22:50:38 +0530

    Created Sign Up Api Route

--- api/index.js
+++ api/index.js
@@ -1,22 +1,27 @@
-import express from 'express';
-import mongoose  from "mongoose";
-import dontenv from 'dotenv';
-import userRouter from '../api/routes/user.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dontenv from "dotenv";
+import userRouter from "../api/routes/user.route.js";
+import authRouter from "../api/routes/auth.route.js"
 
 dontenv.config();
 
-
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connected to the DataBase')
-    }).catch((err)=>{
-    console.log('error')
-    })
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to the DataBase");
+  })
+  .catch((err) => {
+    console.log("error");   
+  });
 
 const app = express();
 
+app.use(express.json())
+
 app.listen(3000, () => {
-    console.log("Server running on Port 3000!!!")
+  console.log("Server running on Port 3000!!!");
 });
 
-app.use('/api/user', userRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter)
