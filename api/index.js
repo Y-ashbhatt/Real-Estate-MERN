4fc030375756a4828926796edcaa8867ac9caea2
Yash Bhatt <30y.bhatt@gmail.com>
2024-08-11 23:47:54 +0530

    Create a middileware to handle possible errors

--- api/index.js
+++ api/index.js
@@ -24,4 +24,16 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
+
+
+// middleware to handle possible errors in sign up
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message
+  });
+});
