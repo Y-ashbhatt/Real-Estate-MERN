df0328bbfc4ef245adaceb6655d5459d104a5d46
Yash Bhatt <30y.bhatt@gmail.com>
2024-08-13 00:14:33 +0530

    Create Sign In Api Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,41 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async ( req, res, next)=>{
-   const { username , email , password } = req.body;
-   //hashing password for DB
-   const hashedPassword = bcryptjs.hashSync(password,10);
-   const newUser = new User({username , email , password:hashedPassword});
-    //try catch for duplicate username or mail error
-    try {
-        await newUser.save();
-   res.status(201).json("User Connected Sucessfully!")
-    } catch (error) {
-        next(error);
-    }
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  //hashing password for DB
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  //try catch for duplicate username or mail error
+  try {
+    await newUser.save();
+    res.status(201).json("User Connected Sucessfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    //check user
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    //check password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    //create a token to store in browser cookie using jwt to hash
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //destucture the password to not send 
+    const {password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
