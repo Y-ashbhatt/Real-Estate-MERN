6dc2c3430f2b801ded52f530fa7beed6393a37b7
Yash Bhatt <0yashbhatt0@gmail.com>
2024-09-19 15:52:32 +0530

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "../api/routes/user.route.js";
 import authRouter from "../api/routes/auth.route.js";
 import listingRouter from "../api/routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dontenv.config();
 
@@ -14,14 +15,15 @@ mongoose
     console.log("Connected to the DataBase");
   })
   .catch((err) => {
-    console.log("error");   
+    console.log("error");
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
 
 app.listen(3000, () => {
   console.log("Server running on Port 3000!!!");
@@ -31,14 +33,19 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, `/client/dist`)));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, `client`, `dist`, `index.html`));
+});
 
 // middleware to handle possible errors in sign up
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
-    message
+    message,
   });
 });
